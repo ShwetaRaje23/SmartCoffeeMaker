f1d5960e00bc5593c3565d16d84c97235970e585
Shweta <Shweta@DN525ati.SUNet>
2015-05-23 16:27:01 -0700

    initial ui

--- server/server/views.py
+++ server/server/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext, loader
 
 def index(request):
 	print "It worked !"
-	template = loader.get_template('index.html')
+	template = loader.get_template('login.html')
 	context = RequestContext(request, {})
 	# event = get_next_event()
 	return HttpResponse(template.render(context))
